4370d168eba31a05863c9dd5d23eb629fc21149a
Okellothomas <thomasolal33@gmail.com>
2022-05-18 08:18:56 +0300

    add the dao

--- src/test/java/model/contactsTest.java
+++ src/test/java/model/contactsTest.java
@@ -7,6 +7,38 @@ import static org.junit.Assert.*;
 
 public class contactsTest {
 
+    //public class SightingTest {
+//
+//    @Test
+//    public void getNameReturnsCorrectName_String() throws Exception {
+//        Sighting newSighting = setNewSighting();
+//        assertEquals("John", newSighting.getName());
+//    }
+//
+//    @Test
+//    public void getNameReturnsCorrectLocation_String() {
+//        Sighting newSighting = setNewSighting();
+//        assertEquals("River", newSighting.getLocation());
+//    }
+//
+//    @Test
+//    public void getNameReturnsCorrectAnimal_int() {
+//        Sighting newSighting = setNewSighting();
+//        assertEquals(34, newSighting.getAnimId());
+//    }
+//
+//    @Test
+//    public void getNameReturnsCorrectEndangered_int() {
+//        Sighting newSighting = setNewSighting();
+//        assertEquals(21, newSighting.getEndangeredId());
+//    }
+//
+//    // the helper classes
+//
+//    public Sighting setNewSighting(){
+//        return new Sighting("John", "River", 34, 21);
+//    }
+
     @Before
     public void setUp() throws Exception {
     }
@@ -14,4 +46,6 @@ public class contactsTest {
     @After
     public void tearDown() throws Exception {
     }
+
+
 }
