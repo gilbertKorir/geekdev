a31af4efcfaaa3ca7dd04a4ceef7c6a3aba2d849
Sylvia-Chepkoech <slycheche@gmail.com>
2022-05-18 08:51:16 +0300

    add styling

---
+++ src/main/java/DAO/Sql2oJobsDao.java
@@ -0,0 +1,33 @@
+package DAO;
+
+import DAO.JobsDao;
+import model.jobs;
+
+import java.util.List;
+
+public class Sql2oJobsDao implements JobsDao {
+    @Override
+    public void add(jobs job) {
+
+    }
+
+    @Override
+    public List<jobs> getAll() {
+        return null;
+    }
+
+    @Override
+    public jobs findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void update(int id, jobs job) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+}
