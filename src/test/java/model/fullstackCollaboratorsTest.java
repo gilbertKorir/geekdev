b4cbba80b7f993db3df5638c1ebb96ff5cc4bb97
gilbertKorir <jerikorir@gmail.com>
2022-05-19 21:00:08 +0300

    ft: generated an Api to get all users in the department

--- src/test/java/model/fullstackCollaboratorsTest.java
+++ src/test/java/model/fullstackCollaboratorsTest.java
@@ -3,11 +3,11 @@ package model;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junitoratore.After;
-import org.junit.Before;
-import org.junit.Test;
-
-import static org.junit.Assert.*;
+//import org.junitoratore.After;
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class fullstackCollaboratorsTest {
