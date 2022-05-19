708613cf97263c8c5280e5c566e897f68ce1b0d3
gilbertKorir <jerikorir@gmail.com>
2022-05-19 12:38:08 +0300

    ft:restructured the hbs

---
+++ src/test/java/dao/Sql2oDevelopersDaoTest.java
@@ -0,0 +1,17 @@
+package dao;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Sql2oDevelopersDaoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
